1176224502a7a43ed2d15486d1bed7b18b2b355c
nghiapt-comartek <100978931+jenazu@users.noreply.github.com>
2022-11-21 18:21:46 +0700

    d

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,6 +1,6 @@
 import { Project } from "../typings";
 
-export const fetchSkills = async () => {
+export const fetchProjects = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
   const data = await res.json();
   const projects: Project[] = data.projects;
